e32a9b224d314b73a35cdd307608bfae9f01cfdb
Garvit Joshi <garvitjoshi9@gmail.com>
2022-02-14 21:23:31 +0530

    Added logs for debugging verseOfTheDay

--- app/Http/Controllers/ReadController.php
+++ app/Http/Controllers/ReadController.php
@@ -14,9 +14,10 @@ class ReadController extends Controller
     public function verseOfTheDay() {
         $bhagavad_count = Bhagavads::all()->count();
         $id_no = rand(0, $bhagavad_count);
+        error_log($id_no);
+        error_log($bhagavad_count);
         $bhagavad = Bhagavads::findOrFail($id_no);
         error_log($bhagavad);
-        error_log(gettype($bhagavad));
         return view('read.verseOfTheDay', [
             'verse' => $bhagavad,
         ]);
